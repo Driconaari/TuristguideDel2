a1d10b93108b234ee8beea7a0cd169ceb457cd91
Asger <asgervb@gmail.com>
2024-03-21 22:24:24 +0100

    finnally fixed pretty much everthing, needs some fine tuning

--- src/main/java/com/example/turistguidedel2/repository/TouristRepository.java
+++ src/main/java/com/example/turistguidedel2/repository/TouristRepository.java
@@ -1,6 +1,8 @@
 package com.example.turistguidedel2.repository;
 
 import com.example.turistguidedel2.model.TouristAttraction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
